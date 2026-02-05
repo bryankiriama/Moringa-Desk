576565f38854361e22841cfe01bf20f41d764b6d
bryankiriama <bryankiriama@gmail.com>
2026-02-05 09:06:07 +0300

    add follow model

--- backend/app/models/__init__.py
+++ backend/app/models/__init__.py
@@ -1,8 +1,9 @@
 from .answer import Answer
+from .follow import Follow
 from .question import Question
 from .question_tag import QuestionTag
 from .tag import Tag
 from .user import User
 from .vote import Vote
 
-__all__ = ["User", "Question", "Tag", "QuestionTag", "Answer", "Vote"]
+__all__ = ["User", "Question", "Tag", "QuestionTag", "Answer", "Vote", "Follow"]
